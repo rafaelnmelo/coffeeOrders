06e3c56a8788a19520013d8038473cb77c6e7cda
Rafael Nunes <rafaelnunesmelo@gmail.com>
2025-01-14 11:52:44 -0300

    Iniciando projeto e organizando pastas

---
+++ CoffeeOrders/CoffeeOrders/CoffeeOrdersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeOrdersApp.swift
+//  CoffeeOrders
+//
+//  Created by Rafael Nunes on 14/01/25.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeOrdersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoffeeOrders/CoffeeOrders/Services/Webservice.swift
@@ -0,0 +1,8 @@
+//
+//  Webservice.swift
+//  CoffeeOrders
+//
+//  Created by Rafael Nunes on 14/01/25.
+//
+
+import Foundation
