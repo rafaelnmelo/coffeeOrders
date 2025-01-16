4ccbee9dfe9ceea2d22ea65a33d62e2a2f05c1b1
Rafael Nunes <rafaelnunesmelo@gmail.com>
2025-01-16 11:40:38 -0300

    OrderCellView e NumberFormatter extension

---
+++ CoffeeOrders/CoffeeOrders/Views/OrderCellView.swift
@@ -0,0 +1,29 @@
+//
+//  OrderCellView.swift
+//  CoffeeOrders
+//
+//  Created by Rafael Nunes on 16/01/25.
+//
+
+import SwiftUI
+
+struct OrderCellView: View {
+    
+    let order: Order
+    
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading) {
+                Text(order.name).accessibilityIdentifier("orderNameText")
+                    .bold()
+                Text("\(order.coffeeName) (\(order.size.rawValue))")
+                    .accessibilityIdentifier("coffeeNameAndSizeText")
+                    .opacity(0.5)
+                
+            }
+            Spacer()
+            Text(order.total as NSNumber, formatter: NumberFormatter.currency)
+                .accessibilityIdentifier("coffeePriceText")
+        }
+    }
+}
